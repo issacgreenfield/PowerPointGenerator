70f86d73c093d7212b72d384d0eb13ee56d7bae5
issacgreenfield <gissac@pdx.edu>
2020-10-14 19:30:03 -0700

    Created basic layout of application.

--- PowerPointGenerator/PowerPointGenerator/Form1.Designer.cs
+++ PowerPointGenerator/PowerPointGenerator/Form1.Designer.cs
@@ -28,13 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.lstvImages = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Slide Title:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Slide Text";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 183);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(687, 273);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(753, 396);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(191, 60);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Search Images";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.BackColor = System.Drawing.Color.Aqua;
+            this.btnCreate.Location = new System.Drawing.Point(1071, 803);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(191, 60);
+            this.btnCreate.TabIndex = 5;
+            this.btnCreate.Text = "Create Slide";
+            this.btnCreate.UseVisualStyleBackColor = false;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // lstvImages
+            // 
+            this.lstvImages.HideSelection = false;
+            this.lstvImages.Location = new System.Drawing.Point(12, 483);
+            this.lstvImages.Name = "lstvImages";
+            this.lstvImages.Size = new System.Drawing.Size(1250, 302);
+            this.lstvImages.TabIndex = 6;
+            this.lstvImages.UseCompatibleStateImageBehavior = false;
+            this.lstvImages.SelectedIndexChanged += new System.EventHandler(this.lstvImages_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1277, 882);
+            this.Controls.Add(this.lstvImages);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.ListView lstvImages;
     }
 }
 
--- PowerPointGenerator/PowerPointGenerator/Form1.cs
+++ PowerPointGenerator/PowerPointGenerator/Form1.cs
@@ -12,9 +12,35 @@ namespace PowerPointGenerator
 {
     public partial class Form1 : Form
     {
+        public List<string> imgLinkList = new List<string>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstvImages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
