060078e50edb45e20a48156dd8f83a6447018a52
issacgreenfield <gissac@pdx.edu>
2020-10-14 20:29:38 -0700

    Added test images

--- PowerPointGenerator/PowerPointGenerator/Form1.cs
+++ PowerPointGenerator/PowerPointGenerator/Form1.cs
@@ -12,7 +12,10 @@ namespace PowerPointGenerator
 {
     public partial class Form1 : Form
     {
+        // Links to the online images
         public List<string> imgLinkList = new List<string>();
+
+            
         public Form1()
         {
             InitializeComponent();
@@ -30,7 +33,19 @@ namespace PowerPointGenerator
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
-
+            // Testing resources
+            ImageList il = new ImageList();
+            il.Images.Add("test1", Image.FromFile(@"C:\Documents\Working\PowerPointExercise\PowerPointGenerator\PowerPointGenerator\images\bg1.jpg"));
+            lstvImages.View = View.LargeIcon;
+            lstvImages.LargeImageList = il;
+            lstvImages.Items.Add("test");
+            for (int i = 0; i < il.Images.Count; i++)
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.ImageIndex = i;
+                lvi.Text = "tester";
+                lstvImages.Items.Add(lvi);
+            }
         }
 
         private void btnCreate_Click(object sender, EventArgs e)
